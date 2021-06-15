b00002c245528f30c3253fe635aedc1752ec7577
HDodge <herbiedodge@gmail.com>
2021-06-15 14:53:21 -0500

    initial commit

---
+++ RecipeMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeMate
+//
+//  Created by Herbert Dodge on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
