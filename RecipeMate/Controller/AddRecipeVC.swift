5ec37e21a4953430bd05c4ec965eee8bb2dcef35
HDodge <herbiedodge@gmail.com>
2021-06-24 13:21:10 -0500

    add ingredients list to AddRecipeVC

--- RecipeMate/Controller/AddRecipeVC.swift
+++ RecipeMate/Controller/AddRecipeVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 protocol AddRecipeVCDelegate: AnyObject {
     func cancelButtonTapped()
@@ -15,7 +16,10 @@ class AddRecipeVC: UIViewController {
     
     @IBOutlet weak var addRecipeView: UIView!
     @IBOutlet weak var saveRecipeButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
+    var controller: NSFetchedResultsController<Ingredient>!
+    let viewModel = IngredientsViewModel()
     weak var delegate: AddRecipeVCDelegate?
 
     override func viewDidLoad() {
@@ -24,12 +28,81 @@ class AddRecipeVC: UIViewController {
     }
 
     func configure() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        initializeIngredients()
         addRecipeView.layer.cornerRadius = 10
         saveRecipeButton.layer.cornerRadius = 10
+        tableView.layer.cornerRadius = 10
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
         delegate?.cancelButtonTapped()
     }
 
+}
+
+extension AddRecipeVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIds.ingredient, for: indexPath) as? IngredientCell else { return UITableViewCell()}
+        configureCell(cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func configureCell(_ cell: IngredientCell, indexPath: IndexPath) {
+        let ingredient = controller.object(at: indexPath)
+        cell.configureCell(ingredient)
+    }
+}
+
+extension AddRecipeVC: NSFetchedResultsControllerDelegate {
+    
+    func initializeIngredients() {
+        let fetchRequest: NSFetchRequest<Ingredient> = Ingredient.fetchRequest()
+        let nameSort = NSSortDescriptor(key: "name", ascending: true)
+        
+        fetchRequest.sortDescriptors = [nameSort]
+        
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: Constants.context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        controller.delegate = self
+        self.controller = controller
+        
+        do {
+            try controller.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        if controller.fetchedObjects?.count == 0 {
+            viewModel.generateIngredients()
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
 }
